edadc3cf40a068b2b1f9644d931b386023d6354d
iamDanaK <daanaa227700@gmail.com>
2024-03-10 14:49:53 +0300

    added my name

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -29,12 +29,12 @@ function App() {
       <div className="card">
         <button onClick={onClick}>count is {count ? count : 'unknown'}</button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Dana <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+  
     </>
   );
 }
