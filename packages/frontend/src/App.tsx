6c07eae62375aac997a8e07addd050eccc4edb32
Farha Alsada <farhaadnan2002@gmail.com>
2024-03-10 15:00:49 +0300

    added my name farha

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
       <div className="card">
         <button onClick={onClick}>count is {count ? count : 'unknown'}</button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Farha Alsada <code>src/App.tsx</code> and save to test HMR
         </p>
         <p> Sayed Ahmed was here </p>
       </div>
