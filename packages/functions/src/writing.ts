12f498c912cd0398e4e18199ae8102a29ca73abd
mzhubail <mzhubail@gmail.com>
2024-04-03 05:27:08 +0300

    Move model invocation to its own function

--- packages/functions/src/writing.ts
+++ packages/functions/src/writing.ts
@@ -12,7 +12,18 @@ export const main: APIGatewayProxyHandlerV2 = async event => {
   }
 
   const prompt = JSON.parse(event.body!)['prompt'];
-  const inputBody = {
+  const text = await runModel(prompt);
+
+  return {
+    statusCode: 200,
+    body: text,
+  };
+};
+
+/** Runs Titan Model for the given prompt and returns its output */
+async function runModel(prompt: string) {
+  // Model parameters
+  const modelParams = {
     inputText: prompt,
     textGenerationConfig: {
       maxTokenCount: 4096,
@@ -22,20 +33,24 @@ export const main: APIGatewayProxyHandlerV2 = async event => {
     },
   };
 
-  const command = new InvokeModelCommand({
-    body: JSON.stringify(inputBody),
-    contentType: 'application/json',
-    accept: '*/*',
-    modelId: 'amazon.titan-text-express-v1',
-  });
+  // Invoke model
+  const response = await client.send(
+    new InvokeModelCommand({
+      body: JSON.stringify(modelParams),
+      contentType: 'application/json',
+      accept: '*/*',
+      modelId: 'amazon.titan-text-express-v1',
+    }),
+  );
 
-  const response = await client.send(command);
+  // Parse model output
   const output = Buffer.from(response.body).toString('utf8');
   const body = JSON.parse(output);
   const text = body.results[0].outputText;
 
-  return {
-    statusCode: 200,
-    body: text,
-  };
-};
+  if (typeof text !== 'string') {
+    throw new Error('Unexpected variable type');
+  }
+
+  return text;
+}
