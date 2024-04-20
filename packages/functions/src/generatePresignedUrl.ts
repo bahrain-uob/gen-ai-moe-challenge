e5fba1c224b49cb365a02d8690020c294d509b65
sahmedkh <ahmedsmahmoodedu@gmail.com>
2024-04-20 18:59:02 +0300

    Modified the code from v2 SDK to v3 SDK

--- packages/functions/src/generatePresignedUrl.ts
+++ packages/functions/src/generatePresignedUrl.ts
@@ -1,45 +1,46 @@
-import { APIGatewayProxyHandlerV2 } from "aws-lambda";
-import { S3 } from "aws-sdk";
+import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
-const s3 = new S3();
-const BUCKET_NAME = process.env.BUCKET_NAME; // Make sure to set this in your environment variables
+const BUCKET_NAME = process.env.BUCKET_NAME;
 
-export const main: APIGatewayProxyHandlerV2 = async (event) => {
+export const main: APIGatewayProxyHandlerV2 = async event => {
   try {
     const fileName = event.queryStringParameters?.fileName;
     const fileType = event.queryStringParameters?.fileType;
+    const questionText = event.queryStringParameters?.questionText;
 
-    if (!fileName || !fileType) {
+    if (!fileName || !fileType || !questionText) {
       return {
         statusCode: 400,
-        body: "Missing fileName or fileType query parameter",
+        body: 'Missing fileName or fileType query parameter',
       };
     }
 
-    const params = {
+    const client = new S3Client();
+    const command = new PutObjectCommand({
       Bucket: BUCKET_NAME,
       Key: fileName,
-      Expires: 60, // URL expires in 60 seconds
       ContentType: fileType,
       Metadata: {
-        question: event.queryStringParameters?.questionText,
+        question: questionText,
       },
-    };
+    });
 
-    // 123
-
-    const presignedPostData = await s3.getSignedUrlPromise("putObject", params);
+    const presignedPostData = await getSignedUrl(client, command, {
+      expiresIn: 3600,
+    });
 
     return {
       statusCode: 200,
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ url: presignedPostData }),
     };
   } catch (err) {
-    console.error("Error generating pre-signed URL", err);
+    console.error('Error generating pre-signed URL', err);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Error generating pre-signed URL" }),
+      body: JSON.stringify({ error: 'Error generating pre-signed URL' }),
     };
   }
 };
