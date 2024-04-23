5596ec13e9090687458326f4275c6b40567f42bd
mzhubail <mzhubail@gmail.com>
2024-04-23 12:55:17 +0300

    Add code to test if the client is authorized
    - I'm testing by posting to `/` since that route is not public.

--- packages/frontend/src/pages/TestPage.tsx
+++ packages/frontend/src/pages/TestPage.tsx
@@ -1,9 +1,23 @@
 import { Link } from 'react-router-dom';
+import { post } from 'aws-amplify/api';
 
 function TestPage() {
+  const testAPIAccess = async () => {
+    const request = post({
+      apiName: 'myAPI',
+      path: '/',
+    });
+
+    const response = await request.response;
+    console.log('Response', response);
+  };
+
   return (
     <>
       <h2> This is a test page</h2>
+      <button onClick={testAPIAccess}>POST /</button>
+      <br />
+      <br />
       <Link to="/"> Back </Link>
     </>
   );
