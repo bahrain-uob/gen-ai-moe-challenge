fc406ca3315d0959a2245c12947d6b62f40a01df
mzhubail <mzhubail@gmail.com>
2024-04-24 23:03:16 +0300

    Remove unused import

--- packages/frontend/src/pages/SignIn.tsx
+++ packages/frontend/src/pages/SignIn.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { signIn, fetchAuthSession } from 'aws-amplify/auth';
+import { signIn } from 'aws-amplify/auth';
 import { Link } from 'react-router-dom';
 
 export default function SignIn() {
