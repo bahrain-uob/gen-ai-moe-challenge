e59f14f4f2eedb3c7872b90d4ad0fb41cfac67ef
Ma-hawaj <ma.hawaj@gmail.com>
2024-04-26 18:58:28 +0300

    Adding the python script the fill the dynamodb with the questions according to the current schema

---
+++ QuestionScript/dynamodbFiller.py
@@ -0,0 +1,46 @@
+import boto3
+import uuid
+import json
+
+client = boto3.client('dynamodb', region_name='us-east-1')
+
+TransactItemss = []
+indexes = {}
+TableName = 'ma.hawaj'   # Channge the table name
+QuestionTypes = ["WritingP1", "WritingP2", "ReadingP1", "ReadingP2", "ReadingP3", "Listening", "SpeakingP1", "SpeakingP2", "SpeakingP3"]
+
+questions = json.loads(open("./questions.json", "r").read())
+
+for type in QuestionTypes:
+    indexes[type] = []
+    for question in questions[type]:
+        id = str(uuid.uuid4())
+        TransactItemss.append({
+            'Put': {
+                'TableName': TableName,
+                'Item': {
+                    'PK': {'S': str(type)},
+                    'SK': {'S': id },
+                    **question
+                }
+            }
+        })
+        indexes[type].append({'S': id}) 
+    
+
+for type in indexes:
+    TransactItemss.append({
+        'Put': {
+            'TableName': TableName,
+            'Item': {
+                'PK': {'S': str(type)},
+                'SK': {'S': 'index'},
+                'index': {'L': indexes[type]}
+          }
+        }
+    })
+    
+response = client.transact_write_items(
+    TransactItems=TransactItemss
+)
+print(response)
