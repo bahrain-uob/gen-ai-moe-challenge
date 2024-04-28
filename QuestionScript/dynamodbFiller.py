d02c58aad80df40a4744515b809f777d215561c7
Ma-hawaj <ma.hawaj@gmail.com>
2024-04-28 23:11:14 +0300

    Adding listening parts in question retrieval lambda and comments to dynamodbFiller.py

--- QuestionScript/dynamodbFiller.py
+++ QuestionScript/dynamodbFiller.py
@@ -4,8 +4,8 @@ import json
 
 client = boto3.client('dynamodb', region_name='us-east-1')
 
-TransactItemss = []
-indexes = {}
+TransactItemss = [] # List of items to be added to the table
+indexes = {} # Dictionary to store the indexes of the questions which contains the list of sort keys of the questions for each type
 TableName = 'ma.hawaj'   # Channge the table name
 QuestionTypes = ["WritingP1", "WritingP2", "ReadingP1", "ReadingP2", "ReadingP3", "ListeningP1",
                  "ListeningP2", "ListeningP3", "ListeningP4", "SpeakingP1", "SpeakingP2", "SpeakingP3"]
