015fb0f4ee88c69bbbc6ba6af863ae37d43b97f0
thatishero <hasanrazi2003@gmail.com>
2024-04-29 09:34:43 +0300

    Updating for styling

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -10,7 +10,7 @@ const Landing = () => (
     </section>
     <div className='lg:pt-32 flex flex-col justify-start'>
       <h3 className='text-[65px] font-extrabold text-[#363534]'>Features</h3>
-      <div className='w-2/5 h-3 bg-[#74ACB5]'></div>
+      <div className='w-2/5 h-3 bg-[#FFFFFF]'></div>
     </div>
     
     <section className="padding">
