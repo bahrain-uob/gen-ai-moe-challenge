29c5d311121841adfa3cfa27b17324396d78ad28
mzhubail <mzhubail@gmail.com>
2024-04-29 12:03:13 +0300

    Introduce `toJSON` to facilitate api access in frontend
    - Add utility function `toJSON` in frontend
    - Simplify `lambda.ts`, which is used for testing purposes in `TestPage`
    - Update `TestPage` to use `toJSON`

---
+++ packages/frontend/src/utilities.ts
@@ -0,0 +1,7 @@
+// Note: I'm using requets type any, because I couldn't find a way to import
+// types from amplify
+export const toJSON = async (request: any) => {
+  const response = await request.response;
+  const json = await response.body.json();
+  return json;
+};
