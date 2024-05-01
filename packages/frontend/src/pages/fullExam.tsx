e868e9454ff3deb02ac1484b29a5749293a297ac
thatishero <hasanrazi2003@gmail.com>
2024-05-02 00:26:27 +0300

    Editing Card components to generalize it

--- packages/frontend/src/pages/fullExam.tsx
+++ packages/frontend/src/pages/fullExam.tsx
@@ -6,7 +6,7 @@ const fullExam = () => {
     <main className="bg-[#FBF9F1] h-screen">
     <Nav/>
     <section className="w-full h-4/6 flex justify-center mt-8">
-      <Card title='IELTS - Full Exam' remTime='34 : 21' startDate='13-04-24'/>
+      <Card title='IELTS - Full Exam' remTime='34 : 21' startDate='13-04-24' timing='Remaining Time'/>
     </section>
 
     <section className="h-1/2">
--- packages/frontend/src/sections/currentExamCard.tsx
+++ packages/frontend/src/sections/currentExamCard.tsx
@@ -1,7 +1,7 @@
 import Progress from '../components/circularProgressBar';
 import Button from '../components/FButton'
 
-const currentExamCard = ({title,remTime,startDate}:{title:string,remTime:string,startDate:string}) => {
+const currentExamCard = ({title,remTime,startDate,timing}:{title:string,remTime:string,startDate:string,timing:string}) => {
   return (
     <div className=" h-full w-3/4 bg-[#FEFDFA] rounded-xl flex flex-col items-center">
 
@@ -13,7 +13,7 @@ const currentExamCard = ({title,remTime,startDate}:{title:string,remTime:string,
         </div>
         <div className=" h-1/6 w-full flex">
             <div className='h-full w-2/6 flex flex-col justify-center gap-y-5 p-6 font-medium'>
-                <h4 className='text-[#989896] text-3xl'>Remaining Time</h4>
+                <h4 className='text-[#989896] text-3xl'>{timing}</h4>
                 <h4 className='text-[#989896] text-3xl'>Start Date</h4>
             </div>
             <div className='h-full w-4/6 flex flex-col justify-center gap-y-5 p-3 font-bold'>
