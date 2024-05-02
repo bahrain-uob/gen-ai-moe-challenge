6a03f282f568246079b7530fc79995125ac84ca8
mzhubail <mzhubail@gmail.com>
2024-05-02 07:09:46 +0300

    Add correct types in `postConfirmation.ts` + minor changes
    - I discovered  that it is easy to find the correct type, just typing
      `postconfir` will show all of the available related types :)
    - Minor changes in formatting

--- packages/functions/src/postConfirmation.ts
+++ packages/functions/src/postConfirmation.ts
@@ -1,13 +1,15 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, UpdateCommand } from '@aws-sdk/lib-dynamodb';
+import { PostConfirmationTriggerHandler } from 'aws-lambda';
 import { Table } from 'sst/node/table';
 
 /**
- * This function is called after a user has been confirmed to intiialize the user in the database
+ * This function is called after a user has been confirmed to intiialize the
+ * user in the database
  *
  * See https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-post-confirmation.html
  */
-const handler = async (event: any, context: any) => {
+const handler: PostConfirmationTriggerHandler = async (event, context) => {
   console.log('Received event:', JSON.stringify(event));
   console.log('Received context:', JSON.stringify(context));
   if (event.triggerSource === 'PostConfirmation_ConfirmSignUp') {
@@ -22,12 +24,13 @@ const handler = async (event: any, context: any) => {
       Key: {
         PK: 'AGGREGATES',
         SK: event.request.userAttributes['custom:Institution'],
-      }, 
+      },
       // if students record does not exist, create it and add user ID to the list
-      UpdateExpression: 'SET students = list_append(if_not_exists(students, :init), :ID)',
+      UpdateExpression:
+        'SET students = list_append(if_not_exists(students, :init), :ID)',
       ExpressionAttributeValues: {
-        ":init": [],
-        ":ID": [event.userName],
+        ':init': [],
+        ':ID': [event.userName],
       },
       ReturnValues: 'NONE',
     });
