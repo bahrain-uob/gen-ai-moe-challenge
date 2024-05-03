a40a79dc645c1f10d8bb74344a5904bbad4951ba
iamDanaK <daanaa227700@gmail.com>
2024-05-03 13:05:57 +0300

    changed the place of qIndex to refer the the correct element in the correctAnswers array.

--- packages/functions/src/GradingReadingListening.ts
+++ packages/functions/src/GradingReadingListening.ts
@@ -1,6 +1,6 @@
 import { DynamoDB } from 'aws-sdk';
 import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
- 
+
 const dynamoDb = new DynamoDB.DocumentClient();
 
 function getEuropeanFrameworkGrade(totalScore: number) {
@@ -16,7 +16,7 @@ function getEuropeanFrameworkGrade(totalScore: number) {
     return 'A2';
   } else {
     return 'A1';
-  } 
+  }
 }
 
 function calculateBandScore(score: number): number {
@@ -54,43 +54,41 @@ function calculateBandScore(score: number): number {
     return 0;
   }
 }
- 
-export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+
+export const handler: APIGatewayProxyHandlerV2 = async event => {
   const { section, sk } = event.pathParameters || {};
   const { studentAnswers } = JSON.parse(event.body || '');
-  if(!sk||!section)
+  if (!sk || !section)
     return {
       statusCode: 400,
       body: JSON.stringify({ error: 'Invalid request' }),
     };
 
-
   const flattenedAnswers = studentAnswers.flat();
   const flattenedStudentAnswers = flattenedAnswers.flat();
 
- 
   console.log('Endpoint received the request:', event);
   console.log('Received student answers:', studentAnswers);
   console.log('Flattened student answers:', flattenedStudentAnswers);
- 
+
   let pks: string[];
- 
+
   if (section === 'reading') {
-    pks = ["ReadingP1", "ReadingP2", "ReadingP3"];
+    pks = ['ReadingP1', 'ReadingP2', 'ReadingP3'];
   } else {
-    pks = ["ListeningP1", "ListeningP2", "ListeningP3", "ListeningP4"];
+    pks = ['ListeningP1', 'ListeningP2', 'ListeningP3', 'ListeningP4'];
   }
- 
+
   try {
     let allQuestions = [];
     let allScores = [];
     let totalScore = 0;
-    let allCorrectAnswers=[];
- 
+    let allCorrectAnswers = [];
+
     for (let index = 0; index < pks.length; index++) {
       const pk = pks[index];
       const answersForPart = studentAnswers[index];
- 
+
       const getItemParams = {
         TableName: process.env.TABLE1_NAME,
         Key: {
@@ -98,19 +96,19 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
           MySortKey: sk,
         },
       };
- 
+
       const partResponse = await dynamoDb.get(getItemParams).promise();
       const partResponseItem = partResponse.Item;
- 
+
       if (partResponseItem && partResponseItem.NumOfQuestions) {
         const numOfQuestions = partResponseItem.NumOfQuestions;
         const questions = [];
- 
+
         for (let i = 0; i < numOfQuestions; i++) {
           questions.push(partResponseItem.Questions[i]);
           const NumOfSubQuestions = questions[i].NumOfSubQuestions;
           const questionType = questions[i].QuestionType;
- 
+
           if (
             questionType === 'Matching Paragraph Information' ||
             questionType === 'True False Not Given' ||
@@ -129,49 +127,49 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
               allScores.push(score);
               allCorrectAnswers.push(correctAnswer);
             }
-          } else {//for other question types: summary completion, table completion, graph completion, multiple select...
+          } else {
+            //for other question types: summary completion, table completion, graph completion, multiple select...
             const correctAnswers = [];
             let qIndex = 0;
- 
+
             for (let j = 0; j < NumOfSubQuestions; j++) {
-              const questionWeight = questions[i].SubQuestions[j].QuestionWeight;
- 
+              const questionWeight =
+                questions[i].SubQuestions[j].QuestionWeight;
+
               for (let k = 0; k < questionWeight; k++) {
-                correctAnswers.push(questions[i].SubQuestions[j].CorrectAnswers[k]);
+                correctAnswers.push(
+                  questions[i].SubQuestions[j].CorrectAnswers[k],
+                );
                 const studentAnswer = answersForPart[i][qIndex];
                 const isCorrect = correctAnswers[qIndex].includes(studentAnswer);
                 const score = isCorrect ? 1 : 0;
                 allScores.push(score);
-                qIndex++;
                 allCorrectAnswers.push(correctAnswers[qIndex]);
+                qIndex++;
               }
             }
           }
         }
- 
+
         allQuestions.push(questions);
-      }     
+      }
     }
 
     for (let i = 0; i < allScores.length; i++) {
       const score = allScores[i];
       totalScore += score;
     }
- 
-   
-    const studentSk= section+sk;
- 
-    console.log("all scores: ", allScores);
-    console.log("all questions: ", allQuestions);
-   
+
+    const studentSk = section + sk;
+
     const europeanFrameworkGrade = getEuropeanFrameworkGrade(totalScore);
     const bandScore = calculateBandScore(totalScore);
- 
+
     // Store the student response in the table
     const putParams = {
       TableName: process.env.TABLE1_NAME,
       Item: {
-        MyPartitionKey: 'student10',
+        MyPartitionKey: 'student4',
         MySortKey: studentSk,
         CorrectAnswers: allCorrectAnswers,
         studentAnswers: flattenedStudentAnswers,
@@ -181,14 +179,13 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
         europeanFrameworkGrade: europeanFrameworkGrade,
       },
     };
- 
+
     await dynamoDb.put(putParams).promise();
- 
+
     return {
       statusCode: 200,
-      body: JSON.stringify({  message: 'Student response stored successfully' }),
+      body: JSON.stringify({ message: 'Student response stored successfully' }),
     };
- 
   } catch (error) {
     console.error('Error retrieving item:', error);
     return {
