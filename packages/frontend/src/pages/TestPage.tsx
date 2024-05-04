847b2db43f9633a7d05960960b5afe25ed48ee92
mzhubail <mzhubail@gmail.com>
2024-05-04 08:28:09 +0300

    Add color palette to `tailwind.config`
    - Add a demo of the colors in `TestPage`

--- packages/frontend/src/pages/TestPage.tsx
+++ packages/frontend/src/pages/TestPage.tsx
@@ -49,8 +49,31 @@ function TestPage() {
       <br />
       <br />
       <Link to="/"> Back </Link>
+
+      <ColorPalette />
     </>
   );
 }
+
+const ColorPalette = () => {
+  const baseClasses = 'w-16 h-16 ';
+
+  return (
+    <>
+      <p> Hover over a color for its name </p>
+      <div className="flex flex-row mt-4">
+        <div className={baseClasses + 'bg-blue-4'} title="blue-4"></div>
+        <div className={baseClasses + 'bg-blue-3'} title="blue-3"></div>
+        <div className={baseClasses + 'bg-blue-2'} title="blue-2"></div>
+        <div className={baseClasses + 'bg-blue-1'} title="blue-1"></div>
+        <div className="ml-4"></div>
+        <div className={baseClasses + 'bg-grey-4'} title="grey-4"></div>
+        <div className={baseClasses + 'bg-grey-3'} title="grey-3"></div>
+        <div className={baseClasses + 'bg-grey-2'} title="grey-2"></div>
+        <div className={baseClasses + 'bg-grey-1'} title="grey-1"></div>
+      </div>
+    </>
+  );
+};
 
 export default TestPage;
