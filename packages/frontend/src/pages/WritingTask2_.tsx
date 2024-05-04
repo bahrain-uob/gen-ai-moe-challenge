3a58de87e28b1c53e1819dca6cab8b34f67e1953
mzhubail <mzhubail@gmail.com>
2024-05-04 09:19:51 +0300

    Initial desgin of Writing 2 page
    - Changed `question` and `answer` to have double new-line

--- packages/frontend/src/pages/WritingTask2_.tsx
+++ packages/frontend/src/pages/WritingTask2_.tsx
@@ -2,13 +2,14 @@ import { CSSProperties, ChangeEvent, FormEvent, useState } from 'react';
 // import { Link } from 'react-router-dom';
 import { WritingGrading, toJSON } from '../utilities';
 import { post } from 'aws-amplify/api';
+import Nav from '../components/Nav';
 
 export function WritingTask2Page_() {
   const [inputs, setInputs] = useState({
     question:
-      'Space exploration is much too expensive and the money should be spent on more important things.\nWhat is your opinion?',
+      'Space exploration is much too expensive and the money should be spent on more important things.\n\nWhat is your opinion?',
     answer:
-      'There is an argument that exploring space is a waste of money and that there are more urgent needs to be addressed on earth, such as reducing poverty and preventing environmental destruction. However, I completely disagree with this opinion for two reasons.\nFirst of all, many of the technologies we take for granted today were originated thanks to space research. Take satellite technology, for example, which we depend on for broadcasting and weather forecasting. Without satellites, we would not be able to follow global events as they happen, nor give populations any warning of approaching storms. Space research has also led to the development of new lightweight materials that offer us heat protection and enable food preservation. Therefore, the challenge of sending human beings into space has often driven the development of new technologies that benefit our everyday lives.\nSecond, we cannot foresee the distant future, so we ought to develop the capability to escape from the earth. Gradually, we are learning how humans can survive for long periods in space and even travel to other planets in the future. If space exploration is halted, this valuable knowledge will never be acquired. It is true that environmental destruction is also a serious issue, but it is also true that we remain dependent on our environment if we never accept the challenge of exploring other worlds.\nIn conclusion, while we undoubtedly face serious problems on our own planet, it is imperative that we continue to explore space. This will promote further technological advances as well as provide a possible means of escape should earth become uninhabitable in future. Ideally, all nations should cooperate in the advancement of space research.',
+      'There is an argument that exploring space is a waste of money and that there are more urgent needs to be addressed on earth, such as reducing poverty and preventing environmental destruction. However, I completely disagree with this opinion for two reasons.\n\nFirst of all, many of the technologies we take for granted today were originated thanks to space research. Take satellite technology, for example, which we depend on for broadcasting and weather forecasting. Without satellites, we would not be able to follow global events as they happen, nor give populations any warning of approaching storms. Space research has also led to the development of new lightweight materials that offer us heat protection and enable food preservation. Therefore, the challenge of sending human beings into space has often driven the development of new technologies that benefit our everyday lives.\n\nSecond, we cannot foresee the distant future, so we ought to develop the capability to escape from the earth. Gradually, we are learning how humans can survive for long periods in space and even travel to other planets in the future. If space exploration is halted, this valuable knowledge will never be acquired. It is true that environmental destruction is also a serious issue, but it is also true that we remain dependent on our environment if we never accept the challenge of exploring other worlds.\n\nIn conclusion, while we undoubtedly face serious problems on our own planet, it is imperative that we continue to explore space. This will promote further technological advances as well as provide a possible means of escape should earth become uninhabitable in future. Ideally, all nations should cooperate in the advancement of space research.',
   });
   const [grading, setGrading] = useState(
     undefined as undefined | WritingGrading,
@@ -64,6 +65,41 @@ export function WritingTask2Page_() {
 
   return (
     <>
+      {/* TODO: use layouts */}
+      <main className="bg-[#FBF9F1] min-h-screen">
+        <Nav />
+        <div className="py-40 px-8">
+          <h1 className="text-2xl text-blue-4 font-bold text-center mb-12">
+            Writing 2
+          </h1>
+
+          <section id="question" className="mb-12">
+            <h2 className="font-bold mb-4">Question:</h2>
+            <p className="whitespace-pre-line ml-4 mb-2">{inputs.question}</p>
+            <div className="flex flex-row-reverse">
+              <span className="bg-white text-blue-4 px-12 py-1 rounded-xl shadow-md">
+                10 points
+              </span>
+            </div>
+          </section>
+
+          <section id="answer">
+            <h2 className="font-bold mb-6">Answer:</h2>
+            <textarea
+              className="w-full h-[50vh] shadow-md resize-none"
+              name="answer"
+              placeholder="Answer"
+              value={inputs.answer}
+              onChange={handleChange}
+            />
+          </section>
+
+          {/* <span className="flex flex-row-reverse rounded-xl bg-white">
+            10 points
+          </span> */}
+        </div>
+      </main>
+
       <h4> A demo page </h4>
 
       <form onSubmit={handleSubmit}>
@@ -76,13 +112,6 @@ export function WritingTask2Page_() {
         />
         <br />
 
-        <textarea
-          name="answer"
-          placeholder="Answer"
-          value={inputs.answer}
-          onChange={handleChange}
-          {...size}
-        />
         <br />
 
         <h4>Feedback</h4>
