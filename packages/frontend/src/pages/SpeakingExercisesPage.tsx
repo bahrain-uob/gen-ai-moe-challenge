37816aae6f4788f078e06b638384d49d54ea726b
mzhubail <mzhubail@gmail.com>
2024-05-05 07:38:48 +0300

    Minor changes in `SpeakingExercisesPage.tsx`

--- packages/frontend/src/pages/SpeakingExercisesPage.tsx
+++ packages/frontend/src/pages/SpeakingExercisesPage.tsx
@@ -4,20 +4,15 @@ import { Link } from 'react-router-dom';
 export const SpeakingExercisesPage: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
-      {' '}
       {/* Changed background color */}
       <div className="flex w-full max-w-6xl mx-auto">
         <div className="w-1/2 p-12 bg-white text-[#3B828E] rounded-lg shadow-md mr-8">
-          {' '}
           {/* Changed background color and added white background */}
-          <h2 className="text-4xl font-bold text-center mb-8">
-            Conversation
-          </h2>{' '}
+          <h2 className="text-4xl font-bold text-center mb-8">Conversation</h2>
           {/* Increased text size */}
           <div className="mt-4 flex justify-center">
             <Link to="/speakingConversation">
               <button className="px-8 py-4 bg-[#3B828E] text-white rounded-lg hover:bg-[#2A5F69] transition duration-300">
-                {' '}
                 {/* Increased button size */}
                 Begin
               </button>
@@ -30,7 +25,6 @@ export const SpeakingExercisesPage: React.FC = () => {
           <div className="mt-4 flex justify-center">
             <Link to="/SpeakingLongQuestion">
               <button className="px-8 py-4 bg-[#3B828E] text-white rounded-lg hover:bg-[#2A5F69] transition duration-300">
-                {' '}
                 {/* Increased button size */}
                 Begin
               </button>
