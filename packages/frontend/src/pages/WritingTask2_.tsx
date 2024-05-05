6a994e10c4a46d241b8841c2f2c1060f9a239daf
mzhubail <mzhubail@gmail.com>
2024-05-05 09:21:29 +0300

    Writing Task 2 Interface - Fetch Feedback

--- packages/frontend/src/pages/WritingTask2_.tsx
+++ packages/frontend/src/pages/WritingTask2_.tsx
@@ -2,6 +2,8 @@ import { ChangeEvent, FormEvent, useState } from 'react';
 // import { Link } from 'react-router-dom';
 import Nav from '../components/Nav';
 import { PointsBadge } from '../components/PointsBadge';
+import { WritingGrading, toJSON } from '../utilities';
+import { post } from 'aws-amplify/api';
 
 export function WritingTask2Page_() {
   const [inputs, setInputs] = useState({
@@ -10,6 +12,8 @@ export function WritingTask2Page_() {
     answer:
       'There is an argument that exploring space is a waste of money and that there are more urgent needs to be addressed on earth, such as reducing poverty and preventing environmental destruction. However, I completely disagree with this opinion for two reasons.\n\nFirst of all, many of the technologies we take for granted today were originated thanks to space research. Take satellite technology, for example, which we depend on for broadcasting and weather forecasting. Without satellites, we would not be able to follow global events as they happen, nor give populations any warning of approaching storms. Space research has also led to the development of new lightweight materials that offer us heat protection and enable food preservation. Therefore, the challenge of sending human beings into space has often driven the development of new technologies that benefit our everyday lives.\n\nSecond, we cannot foresee the distant future, so we ought to develop the capability to escape from the earth. Gradually, we are learning how humans can survive for long periods in space and even travel to other planets in the future. If space exploration is halted, this valuable knowledge will never be acquired. It is true that environmental destruction is also a serious issue, but it is also true that we remain dependent on our environment if we never accept the challenge of exploring other worlds.\n\nIn conclusion, while we undoubtedly face serious problems on our own planet, it is imperative that we continue to explore space. This will promote further technological advances as well as provide a possible means of escape should earth become uninhabitable in future. Ideally, all nations should cooperate in the advancement of space research.',
   });
+  // Feedback from backend
+  const [grading, setGrading] = useState<undefined | WritingGrading>(undefined);
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setInputs({ ...inputs, [e.target.name]: e.target.value });
@@ -17,7 +21,24 @@ export function WritingTask2Page_() {
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    console.log(inputs);
+    const response = await toJSON(
+      post({
+        apiName: 'myAPI',
+        path: '/grade-writing',
+        options: {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: {
+            writingTask: 'Task 2',
+            ...inputs,
+          },
+        },
+      }),
+    );
+    console.log(response);
+    setGrading(response);
   };
 
   return (
