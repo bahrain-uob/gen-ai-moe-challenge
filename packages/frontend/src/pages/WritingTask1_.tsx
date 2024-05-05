e2d3eb3b11493b870a0ee8b40c1fe765c67a469c
mzhubail <mzhubail@gmail.com>
2024-05-05 15:15:15 +0300

    Switch both writing interfaces to use websocket

--- packages/frontend/src/pages/WritingTask1_.tsx
+++ packages/frontend/src/pages/WritingTask1_.tsx
@@ -2,14 +2,16 @@ import { ChangeEvent, FormEvent, useState } from 'react';
 // import { Link } from 'react-router-dom';
 import Nav from '../components/Nav';
 import { PointsBadge } from '../components/PointsBadge';
-import { WritingGrading, toJSON } from '../utilities';
+import { WritingGrading } from '../utilities';
 import { WritingFeedbackContainer } from '../components/WritingFeedback';
-import { post } from 'aws-amplify/api';
+import useWebSocket from 'react-use-websocket';
 
 export function WritingTask1Page_() {
   const [inputs, setInputs] = useState({
     question:
       'The chart below shows the number of men and women in further education in Britain in three periods and whether they were studying fulltime or part-time.\n\nSummarise the information by selecting and reporting the main features, and make comparisons where relevant.',
+    graphDescription:
+      'Period: 1970/71\n- Number of men: 1000 as part-time and 100 as full-time\n- Number of women: 700 as part-time and 50 as full-time\n\nPeriod: 1980/81\n- Number of men: 850 as part-time and 150 as full-time\n- Number of women: 800 as part-time and 200 as full-time\n\nPeriod: 1990/91\n- Number of men: 900 as part-time and 225 as full-time\n- Number of women: 1100 as part-time and 225 as full-time',
     answer:
       'This is a bar chart of the number of men and women in further education in Britain in three periods.  In 1970, Most of Men were studying part-time but from 1980, studying part-time was decreased and studying full-time was increased and in 1990, it was twice as many students as in 1970.  On the other hand, women studying Full-time were increased and not only Full-time, part-time also were increased, in 1990, Studying full-time was three times as many students as in 1970.  If compare Men and Women, as you see, in 1970, Men were Studying more than women full-time or part-time but it changed from 1980 and then, in 1990, Women were studying part-time more than Men and Studying full-time was same number.  It shows you Women has a high education now.',
   });
@@ -20,26 +22,32 @@ export function WritingTask1Page_() {
     setInputs({ ...inputs, [e.target.name]: e.target.value });
   };
 
+  const { sendMessage } = useWebSocket(
+    // Get the websocket url from the environment
+    import.meta.env.VITE_WEBSOCKET_URL as string,
+    {
+      onOpen: event => console.log('opened', event),
+      onClose: event => console.log('closed', event),
+      onMessage: e => {
+        console.log('event', e);
+        const response = JSON.parse(e.data);
+        console.log('message', response);
+        if (response['Coherence & Cohesion']) {
+          setGrading(response);
+        }
+      },
+      shouldReconnect: () => true,
+    },
+  );
+
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    const response = await toJSON(
-      post({
-        apiName: 'myAPI',
-        path: '/grade-writing',
-        options: {
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: {
-            writingTask: 'Task 1',
-            ...inputs,
-          },
-        },
+    sendMessage(
+      JSON.stringify({
+        action: 'gradeWriting',
+        data: { writingTask: 'Task 1', ...inputs },
       }),
     );
-    console.log(response);
-    setGrading(response);
   };
 
   return (
