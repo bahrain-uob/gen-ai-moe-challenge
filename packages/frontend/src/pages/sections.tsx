9a6e8c70d32319468cf42a9c4f9ac3f9b708c7cb
thatishero <hasanrazi2003@gmail.com>
2024-05-06 08:46:18 +0300

    Hiding dummy page with link to reading section

--- packages/frontend/src/pages/sections.tsx
+++ packages/frontend/src/pages/sections.tsx
@@ -4,9 +4,18 @@ import Button from '../components/TButton';
 const sections = () => (
   <main className="bg-[#FBF9F1] h-screen">
     <Nav />
-    <div className="py-32">
-      <h4 className="text-xl mb-4"> Better pages </h4>
-      <div className="flex">
+    <div className="pt-32">
+      <h4 className="text-xl mb-4 hidden"> Better pages </h4>
+      <div className="flex gap-5 justify-center">
+        <Link to="">
+          <Button label="Listening" />
+        </Link>
+        <Link to="">
+          <Button label="Speaking" />
+        </Link>
+        <Link to="/reading/1">
+          <Button label="Reading" />
+        </Link>
         <Link to="/_writing-task1">
           <Button label="Writing Task 1" />
         </Link>
@@ -14,6 +23,7 @@ const sections = () => (
           <Button label="Writing Task 2" />
         </Link>
       </div>
+      <div className='hidden'>
       <h4 className="text-xl mb-4"> Dummy pages </h4>
       <div className="flex">
         <Link to="">
@@ -32,6 +42,8 @@ const sections = () => (
           <Button label="Writing Task 2" />
         </Link>
       </div>
+
+    </div>
     </div>
   </main>
 );
