2ef9d41dc6d70ccd2863e717317c9b7ff99273a6
mzhubail <mzhubail@gmail.com>
2024-05-06 16:48:45 +0300

    Change structure of `App.tsx`
    - Move everything inside a section

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -20,19 +20,20 @@ const App = () => {
       <Nav />
       <section className="xl:padding-1 wide:padding-r">
         <Describe />
-      </section>
-      <div className="lg:pt-32 flex flex-col justify-start pl-3 max-sm:pt-20">
-        <h3 className="text-[65px] font-extrabold text-[#363534] max-sm:text-[40px]">
-          Features
-        </h3>
-        <div className="w-2/5 h-3 bg-[#74ACB5]"></div>
-      </div>
 
-      <section className="py-12">
-        <Features />
-      </section>
+        <div className="lg:pt-32 flex flex-col justify-start pl-3 max-sm:pt-20">
+          <h3 className="text-[65px] font-extrabold text-[#363534] max-sm:text-[40px]">
+            Features
+          </h3>
+          <div className="w-2/5 h-3 bg-[#74ACB5]"></div>
+        </div>
 
-      <button onClick={signOutHandler}> Sign out </button>
+        <section className="py-12">
+          <Features />
+        </section>
+
+        <button onClick={signOutHandler}> Sign out </button>
+      </section>
     </main>
   );
 };
