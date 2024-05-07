409c87a0ac9851c03f99137614fd64b4db8e32ec
mzhubail <mzhubail@gmail.com>
2024-05-07 16:05:43 +0300

    Add code to display confidence levels

--- packages/frontend/src/pages/PLTestPage.tsx
+++ packages/frontend/src/pages/PLTestPage.tsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
 import { Option, Question } from './PlacementTest';
 import { sections as questions } from './Questions';
 
+interface CF {
+  A1: number;
+  A2: number;
+  B1: number;
+  B2: number;
+  C1: number;
+  C2: number;
+}
+
 export const PLTestPage = () => {
   const rQuestion = getRandomQuestion();
+  const [cf, setCf] = useState({
+    A1: 0.05,
+    A2: 0.1,
+    B1: 0.2,
+    B2: 0.3,
+    C1: 0.6,
+    C2: 1,
+  });
 
-  return <RenederQuestion question={rQuestion} handleClick={console.log} />;
+  return (
+    <>
+      <DevPanel cf={cf} question={rQuestion} />
+      <RenederQuestion question={rQuestion} handleClick={console.log} />
+    </>
+  );
 };
 
 const optionsStyle =
@@ -43,4 +66,31 @@ const getRandomQuestion = () => {
   const qArray = questions[Math.floor(Math.random() * questions.length)];
   const question = qArray[Math.floor(Math.random() * qArray.length)];
   return question;
+};
+
+const DevPanel = ({ cf, question }: { cf: CF; question: Question }) => {
+  const correctAnswer = question.options.findIndex(
+    ({ isCorrect }) => isCorrect,
+  );
+
+  return (
+    <>
+      <div className="mb-16">
+        <span>Correct answer: {correctAnswer + 1}</span>
+        <div>
+          {Object.entries(cf).map(([level, prop]) => (
+            <div className="flex items-center mb-1">
+              <div className="w-16">
+                {level}, {prop}
+              </div>
+              <div
+                className="inline-block bg-blue-4 h-4 ml-2"
+                style={{ width: 10 * prop + 'rem' }}
+              ></div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
