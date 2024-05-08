523cab0feaf02cbf44af1258cfb05047b1ec23ee
mzhubail <mzhubail@gmail.com>
2024-05-08 13:24:41 +0300

    Revert "Add shuffling"
    This reverts commit 376725f05181b5a964c7c4a7479dc8db29eaf9e1.

--- packages/frontend/src/pages/PLTestPage.tsx
+++ packages/frontend/src/pages/PLTestPage.tsx
@@ -116,7 +116,6 @@ const DevPanel = ({
 
 const selectLevel = (cf: number[]): number => {
   const cfObj = Object.entries(cf);
-  shuffle(cfObj);
 
   // Sort in descending order
   cfObj.sort(([_, a], [__, b]) => {
@@ -153,21 +152,3 @@ const selectLevel = (cf: number[]): number => {
   // });
   // return ind;
 };
-
-/* See https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array */
-function shuffle(array: any[]) {
-  let currentIndex = array.length;
-
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-    // Pick a remaining element...
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-}
