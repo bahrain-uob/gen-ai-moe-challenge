22f76e8498e71f019d280d5411bde103b814a587
iamDanaK <daanaa227700@gmail.com>
2024-05-10 15:24:18 +0300

    just removed an unused import

--- packages/frontend/src/components/Reading/ListSelectionQuestionComponent.tsx
+++ packages/frontend/src/components/Reading/ListSelectionQuestionComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { QuestionListSelection } from '../../utilities/readingUtilities';
 
 export const ListSelectionQuestionComponent = ({
