6bc402ba757783baa3b869cf783984f12eec82af
mzhubail <mzhubail@gmail.com>
2024-05-10 11:38:18 +0300

    Add context to handle authentication related info

---
+++ packages/frontend/src/AuthContext.tsx
@@ -0,0 +1,40 @@
+import {
+  AuthSession,
+  AuthUser,
+  fetchAuthSession,
+  getCurrentUser,
+} from 'aws-amplify/auth';
+import { createContext, useState } from 'react';
+
+declare var __authInterval: any;
+
+type AuthInfo = {
+  authSession: AuthSession;
+  user: AuthUser;
+};
+
+export const createAuthContext = () => {
+  /** Stores `AuthInfo` for the current session */
+  const AuthContext = createContext<AuthInfo | undefined>(undefined);
+  /** This is used to update the `AuthInfo` */
+  const [authInfo, setAuthInfo] = useState<AuthInfo | undefined>(undefined);
+  // Note that we bind the state to the `AuthInfo` value provided
+  const provider = <AuthContext.Provider value={authInfo} />;
+
+  /** Updates the current `AuthInfo` */
+  const triggerUpdate = async () => {
+    const authSession = await fetchAuthSession({ forceRefresh: true }); // try to refresh the session first
+    const user = await getCurrentUser();
+
+    setAuthInfo({ authSession, user });
+  };
+
+  // Set update interval
+  if (__authInterval) {
+    clearInterval(__authInterval);
+  }
+  __authInterval = setInterval(triggerUpdate, 5000);
+
+  return { authInfoProvider: provider, updateAuthInfo: triggerUpdate };
+};
+
