b56fe47b15a118d65ce026e34e54b29795577263
thatishero <hasanrazi2003@gmail.com>
2024-05-11 21:57:50 +0300

    Coding style by prettier

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,34 +2,34 @@ import Describe from './sections/toolDescribe';
 import Features from './sections/features';
 import './index.css';
 import { Link } from 'react-router-dom';
-import Nav from './components/landingNav'
+import Nav from './components/landingNav';
 
 const App = () => {
   return (
     <>
-    <main className='bg-grey-1'>
-      <section>
-        <Nav/>
-      </section>
-      <section className="xl:padding-1 wide:padding-r">
-        <Describe />
-
-        <div className="pt-16 flex flex-col justify-start pl-3">
-          <h3 className="text-[65px] font-extrabold text-[#363534] max-sm:text-[40px]">
-            Features
-          </h3>
-          <div className="w-2/5 h-3 bg-[#74ACB5]"></div>
-        </div>
-
-        <section className="py-12">
-          <Features />
+      <main className="bg-grey-1">
+        <section>
+          <Nav />
         </section>
+        <section className="xl:padding-1 wide:padding-r">
+          <Describe />
 
-        <Link to="sign-out">
-          <button> Sign out </button>
-        </Link>
-      </section>
-    </main>
+          <div className="pt-16 flex flex-col justify-start pl-3">
+            <h3 className="text-[65px] font-extrabold text-[#363534] max-sm:text-[40px]">
+              Features
+            </h3>
+            <div className="w-2/5 h-3 bg-[#74ACB5]"></div>
+          </div>
+
+          <section className="py-12">
+            <Features />
+          </section>
+
+          <Link to="sign-out">
+            <button> Sign out </button>
+          </Link>
+        </section>
+      </main>
     </>
   );
 };
