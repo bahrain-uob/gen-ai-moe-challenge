b4f11ea86928790079822408f9896b543e5740a2
mzhubail <mzhubail@gmail.com>
2024-05-12 14:50:31 +0300

    Fromat `WritingFeedback` and fix issue with grammarMistakes
    - Handle the case of empty array

--- packages/frontend/src/components/WritingFeedback.tsx
+++ packages/frontend/src/components/WritingFeedback.tsx
@@ -9,48 +9,67 @@ export const WritingFeedbackContainer = ({
   const pClassname = 'whitespace-pre-line ml-4 mb-8';
   const headingStyle = 'font-light mb-2';
 
-  
   let grammarMistakes = null;
   if (feedback['Grammer Tool Feedback']) {
-    grammarMistakes = feedback['Grammer Tool Feedback'].map((mistake, index) => {
-      const context = mistake.context.text;
-      const before = context.substring(0, mistake.context.offset);
-      const inner = context.substring(mistake.context.offset, mistake.context.offset + mistake.context.length);
-      const after = context.substring(mistake.context.offset + mistake.context.length);
+    grammarMistakes = feedback['Grammer Tool Feedback'].map(
+      (mistake, index) => {
+        const context = mistake.context.text;
+        const before = context.substring(0, mistake.context.offset);
+        const inner = context.substring(
+          mistake.context.offset,
+          mistake.context.offset + mistake.context.length,
+        );
+        const after = context.substring(
+          mistake.context.offset + mistake.context.length,
+        );
 
-      let body = [];
-      for (let x in mistake) {
-        body.push(<p key={x}>{x.toString()}:  {JSON.stringify(mistake[x])}</p>);
-      };
+        let body = [];
+        for (let x in mistake) {
+          body.push(
+            <p key={x}>
+              {x.toString()}: {JSON.stringify(mistake[x])}
+            </p>,
+          );
+        }
 
-      const title = (
-        <>
-          <span>{before.trim()} </span>
-          <span className="bg-yellow-300">{inner.trim()}</span>
-          <span> {after.trim()}</span>
-        </>
-      );
+        const title = (
+          <>
+            <span>{before.trim()} </span>
+            <span className="bg-yellow-300">{inner.trim()}</span>
+            <span> {after.trim()}</span>
+          </>
+        );
 
-      return (
-        <CollapsableCard title={title} key={index}>
-          {body}
-        </CollapsableCard>
-      );
-    });
+        return (
+          <CollapsableCard title={title} key={index}>
+            {body}
+          </CollapsableCard>
+        );
+      },
+    );
   }
 
   return (
     <>
-      <h5 className={headingStyle}>Coherence & Cohesion</h5>
-      <p className={pClassname}>{feedback['Coherence & Cohesion']}</p>
-      <h5 className={headingStyle}>Grammatical Range & Accuracy</h5>
-      <p className={pClassname}>{feedback['Grammatical Range & Accuracy']}</p>
-      <h5 className={headingStyle}>Lexical Resource</h5>
-      <p className={pClassname}>{feedback['Lexical Resource']}</p>
       <h5 className={headingStyle}>Task Responce</h5>
       <p className={pClassname}>{feedback['Task Responce']}</p>
+
+      <h5 className={headingStyle}>Coherence & Cohesion</h5>
+      <p className={pClassname}>{feedback['Coherence & Cohesion']}</p>
+
+      <h5 className={headingStyle}>Grammatical Range & Accuracy</h5>
+      <p className={pClassname}>{feedback['Grammatical Range & Accuracy']}</p>
+
+      <h5 className={headingStyle}>Lexical Resource</h5>
+      <p className={pClassname}>{feedback['Lexical Resource']}</p>
+
       <h5 className={headingStyle}>Grammar Mistakes</h5>
-      {grammarMistakes ? grammarMistakes : <p className={pClassname}>No Mistakes Found</p>}
+      {grammarMistakes && grammarMistakes.length !== 0 ? (
+        grammarMistakes
+      ) : (
+        <p className={pClassname}>No Mistakes Found</p>
+      )}
+
       <h5 className={headingStyle}>Combined Feedback</h5>
       <p className={pClassname}>{feedback['Combined Feedback']}</p>
     </>
