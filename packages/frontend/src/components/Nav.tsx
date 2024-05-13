5774aaffd43506b9a9d3c814e88b6a42d3699d73
mzhubail <mzhubail@gmail.com>
2024-05-13 14:48:08 +0300

    Add working menu in mobile

--- packages/frontend/src/components/Nav.tsx
+++ packages/frontend/src/components/Nav.tsx
@@ -1,4 +1,5 @@
-import { BsList } from 'react-icons/bs';
+import { useState } from 'react';
+import { BsArrowRightCircleFill, BsList } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
 const linkStyling =
@@ -28,12 +29,48 @@ const Nav = () => {
             <img className="w-11" src="assets/User.png" />
           </Link>
 
-          <Link className={linkStyling + 'md:hidden ml-auto'} to="">
-            <BsList size="35" />
-          </Link>
+          <MobileMenu className={linkStyling + 'md:hiddent ml-auto'} />
         </div>
       </nav>
     </header>
+  );
+};
+
+const MobileMenu = ({ className = '' }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(s => !s);
+
+  return (
+    <>
+      <button className={className} onClick={() => toggleMenu()}>
+        <BsList size="35" />
+      </button>
+      <div
+        className={`
+          h-screen bg-grey-1 fixed top-0 right-0 z-50 ${
+            isOpen ? 'max-w-[40vw] ' : 'max-w-0'
+          } transition-all duration-300 overflow-hidden`}
+      >
+        <div className={containerStyling + 'flex-col w-[40vw]'}>
+          <button
+            className={linkStyling + 'py-3 flex-row text-gray-700'}
+            onClick={() => toggleMenu()}
+          >
+            <BsArrowRightCircleFill className="mr-3" />
+            <span>Back</span>
+          </button>
+          <button className={linkStyling + 'py-3 flex-row text-gray-700'}>
+            <span>Sign out</span>
+          </button>
+        </div>
+      </div>
+      <div
+        className={`bg-black ${
+          isOpen ? 'bg-opacity-55 z-20' : 'bg-opacity-0 -z-10'
+        } h-screen w-screen fixed top-0 left-0 transition-all duration-300`}
+      ></div>
+    </>
   );
 };
 
