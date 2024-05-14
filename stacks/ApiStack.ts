110886cb6b0572a526952543991170df60d2460a
Ma-hawaj <ma.hawaj@gmail.com>
2024-05-14 17:04:47 +0300

    Added submit

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -188,6 +188,15 @@ export function ApiStack({ stack }: StackContext) {
           },
         },
       },
+      fullTestSubmit: {
+        function: {
+          handler: 'packages/functions/src/websockets/fullTest/submit.main',
+          timeout: '120 seconds',
+          environment: {
+            grammerToolDNS: grammarToolDNS,
+          },
+        },
+      },
       speaking: {
         function: {
           handler: 'packages/functions/src/speaking.main',
