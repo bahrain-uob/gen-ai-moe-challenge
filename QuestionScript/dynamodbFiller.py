972c70d75b597d05b184e2867376986ddc8c2fbe
Bilal <xvBilalvx@gmial.com>
2024-05-17 17:20:28 +0300

    added the questions in the script

--- QuestionScript/dynamodbFiller.py
+++ QuestionScript/dynamodbFiller.py
@@ -6,9 +6,10 @@ client = boto3.client('dynamodb', region_name='us-east-1')
 
 TransactItemss = [] # List of items to be added to the table
 indexes = {} # Dictionary to store the indexes of the questions which contains the list of sort keys of the questions for each type
-TableName = 'ma.hawaj'   # Channge the table name
+TableName = 'Bilal-codecatalyst-sst-app-Records'   # Channge the table name
 QuestionTypes = ["WritingP1", "WritingP2", "ReadingP1", "ReadingP2", "ReadingP3", "ListeningP1",
-                 "ListeningP2", "ListeningP3", "ListeningP4", "SpeakingP1", "SpeakingP2P3"]
+                 "ListeningP2", "ListeningP3", "ListeningP4", "SpeakingP1", "SpeakingP2P3","vocabA1","vocabA2","vocabB1",
+                 "vocabB2","vocabC1","vocabC2"]
 
 questions = json.loads(open("./questions.json", "r", encoding="utf8").read())
 
