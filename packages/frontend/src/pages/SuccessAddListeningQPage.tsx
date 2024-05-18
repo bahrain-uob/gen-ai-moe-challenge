6b17bc4aeaa939cb7ddc058415b41d92a7a95182
ErasedKyte <kyouku9900@gmail.com>
2024-05-18 14:48:55 +0300

    fix

--- packages/frontend/src/pages/SuccessAddListeningQPage.tsx
+++ packages/frontend/src/pages/SuccessAddListeningQPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import WaveSurferPlayer from './waveformListeningTest';
 
