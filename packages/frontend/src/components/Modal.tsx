8bad3d5122da0826751d480bb735bda16112bed8
mzhubail <mzhubail@gmail.com>
2024-05-19 02:01:08 +0300

    Adapt style to help material

--- packages/frontend/src/components/Modal.tsx
+++ packages/frontend/src/components/Modal.tsx
@@ -1,4 +1,5 @@
 import { MouseEventHandler } from 'react';
+import { BsQuestionLg } from 'react-icons/bs';
 
 type ModalProps = { isOpen: boolean; onCancel?: MouseEventHandler };
 
@@ -36,28 +37,15 @@ export const Modal: React.FC<ModalProps> = ({ isOpen, onCancel }) => {
           >
             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">
-                <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                  <svg
-                    className="h-6 w-6 text-red-600"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-                    />
-                  </svg>
+                <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-sky-100 sm:mx-0 sm:h-10 sm:w-10">
+                  <BsQuestionLg className="text-sky-600" size={18} />
                 </div>
                 <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                   <h3
                     className="text-base font-semibold leading-6 text-gray-900"
                     id="modal-title"
                   >
-                    Deactivate account
+                    Instructions
                   </h3>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
@@ -72,16 +60,10 @@ export const Modal: React.FC<ModalProps> = ({ isOpen, onCancel }) => {
             <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
               <button
                 type="button"
-                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-              >
-                Deactivate
-              </button>
-              <button
-                type="button"
                 className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                 onClick={onCancel}
               >
-                Cancel
+                Return
               </button>
             </div>
           </div>
