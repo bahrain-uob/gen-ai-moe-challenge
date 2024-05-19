6126e30b431cbaa121d86b431238cef8895a38f2
mzhubail <mzhubail@gmail.com>
2024-05-19 15:38:41 +0300

    Improve message + `ConfirmFullTestStart` takes `onConfirm`

--- packages/frontend/src/components/ConfirmFullTestStart.tsx
+++ packages/frontend/src/components/ConfirmFullTestStart.tsx
@@ -1,3 +1,16 @@
-export const ConfirmFullTestStart = () => {
-  return <p>Are you sure you want to start?</p>;
+import { MouseEventHandler } from 'react';
+
+interface ConfirmFullTestStartProps {
+  onConfirm: MouseEventHandler;
+}
+
+export const ConfirmFullTestStart: React.FC<ConfirmFullTestStartProps> = ({
+  onConfirm,
+}) => {
+  return (
+    <div>
+      <p>Are you sure you want to start?</p>
+      <button onClick={onConfirm}> Yes </button>
+    </div>
+  );
 };
--- packages/frontend/src/pages/FullTestPage.tsx
+++ packages/frontend/src/pages/FullTestPage.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { Layout } from '../Layout';
 import { ConfirmFullTestStart } from '../components/ConfirmFullTestStart';
-import useWebSocket from 'react-use-websocket';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 import { useSocketUrl } from '../utilities';
 import { useState } from 'react';
 
@@ -9,18 +9,9 @@ export const FullTestPage = () => {
   const [state, setState] = useState<any>(null);
   const { testId } = useParams();
 
-  const socketUrl = useSocketUrl();
-  if (!socketUrl) {
-    return (
-      <Layout>
-        {`We have an issue connecting with the server :(`}
-        <br />
-        Please try again later
-      </Layout>
-    );
-  }
+  const socketUrl = useSocketUrl() ?? '';
 
-  const {} = useWebSocket(socketUrl, {
+  const { readyState } = useWebSocket(socketUrl, {
     onOpen: event => console.log('opened', event),
     onClose: event => console.log('closed', event),
     onMessage: e => {
@@ -32,11 +23,34 @@ export const FullTestPage = () => {
     shouldReconnect: () => true,
   });
 
-  if (!testId) {
+  if (readyState !== ReadyState.OPEN)
     return (
       <Layout>
-        <ConfirmFullTestStart />
+        <p>
+          {socketUrl === ''
+            ? 'You have to sign in first'
+            : connectionStatus[readyState]}
+        </p>
+      </Layout>
+    );
+
+  if (!testId) {
+    const startTest = () => {};
+
+    return (
+      <Layout>
+        <ConfirmFullTestStart onConfirm={() => startTest()} />
       </Layout>
     );
   }
+};
+
+const connectionStatus = {
+  [ReadyState.CONNECTING]: "We're trying to connect to the server...",
+  [ReadyState.OPEN]: 'Open',
+  [ReadyState.CLOSING]:
+    'Failed to connect to the server.  Please try again later',
+  [ReadyState.CLOSED]:
+    'Failed to connect to the server.  Please try again later',
+  [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
 };
