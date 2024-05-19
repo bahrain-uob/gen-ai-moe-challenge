7c42bb8a5fac3e29b0d63d7aa55d2b9507265f4f
ErasedKyte <kyouku9900gmail.com>
2024-05-19 23:01:48 +0300

    added hardcodded component for waveform demo

--- packages/frontend/src/pages/SuccessAddListeningQPage.tsx
+++ packages/frontend/src/pages/SuccessAddListeningQPage.tsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
+import WaveSurfer from 'wavesurfer.js';
 
 export const SuccessAddListeningQPage: React.FC = () => {
   const location = useLocation();
   const [isLoading, setIsLoading] = useState(true);
   const [audioUrl, setAudioUrl] = useState('');
+  const [audioPlayed, setAudioPlayed] = useState(false);
+  const wavesurferContainerRef = useRef<HTMLDivElement | null>(null);
+  const wavesurferRef = useRef<WaveSurfer | null>(null);
 
   useEffect(() => {
     if (location.state?.response) {
@@ -13,18 +17,51 @@ export const SuccessAddListeningQPage: React.FC = () => {
       setIsLoading(false);
     }
   }, [location.state]);
+  const handlePlay = () => {
+    if (wavesurferRef.current) {
+      wavesurferRef.current.play();
+      setAudioPlayed(true);
+    }
+  };
+
+  useEffect(() => {
+    if (audioUrl && wavesurferRef.current) {
+      const wavesurfer = WaveSurfer.create({
+        container: wavesurferRef.current,
+        waveColor: 'rgb(59, 130, 142)',
+        progressColor: 'rgb(59, 200, 200)',
+        height: 200,
+        interact: false,
+      });
+      wavesurfer.load(audioUrl);
+      wavesurfer.on('ready', () => {});
+      wavesurferRef.current = wavesurfer;
+      wavesurferRef.current.load(audioUrl);
+      wavesurferRef.current.on('ready', () => {});
+    }
+
+    return () => {
+      // Clean up the wavesurfer instance on component unmount
+      if (wavesurferRef.current) {
+        wavesurferRef.current.destroy();
+        wavesurferRef.current = null;
+      }
+    };
+  }, [audioUrl]);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
-
   return (
     <div>
       <h1>Audio</h1>
-      <audio controls>
-        <source src={audioUrl} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
+      <div ref={wavesurferRef} style={{ width: '100%', height: '200px' }} />
+      {!audioPlayed && <button onClick={handlePlay}>Play</button>}{' '}
+      <div
+        ref={wavesurferContainerRef}
+        style={{ width: '100%', height: '200px' }}
+      />
+      {!audioPlayed && <button onClick={handlePlay}>Play</button>}
     </div>
   );
 };
