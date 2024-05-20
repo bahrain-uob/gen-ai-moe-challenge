352fa0a1c6d11548446667d80a21bdd587db2beb
sahmedkh <ahmedsmahmoodedu@gmail.com>
2024-05-20 08:13:25 +0300

    Added micRecorder custom hook The current implementation displays the audio URL. This was done purely for testing purposes.

--- packages/frontend/src/components/MicButton.tsx
+++ packages/frontend/src/components/MicButton.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { BsFillMicFill, BsFillMicMuteFill } from 'react-icons/bs';
+import { useMicRecorder } from './useMicRecorder';
 
 type MicButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
@@ -7,6 +8,8 @@ export const MicButton: React.FC<MicButtonProps> = ({
   className,
   ...props
 }) => {
+  const { isRecording, startRecording, stopRecording, audioURL } =
+    useMicRecorder();
   const [isOn, setOn] = useState(false);
   const size = 28;
 
@@ -18,12 +21,22 @@ export const MicButton: React.FC<MicButtonProps> = ({
   const style = isOn ? 'bg-red-400' : 'bg-white';
 
   return (
-    <button
-      {...props}
-      className={`${className} inline-block ${style} p-4 rounded-full border-2 transition-all duration-200`}
-      onClick={() => setOn(x => !x)}
-    >
-      {icon}
-    </button>
+    <>
+      <button
+        {...props}
+        className={`${className} inline-block ${style} p-4 rounded-full border-2 transition-all duration-200`}
+        onClick={() => {
+          setOn(x => !x);
+          isRecording ? stopRecording() : startRecording();
+        }}
+      >
+        {icon}
+      </button>
+      {audioURL && (
+        <div>
+          <audio controls src={audioURL}></audio>
+        </div>
+      )}
+    </>
   );
 };
---
+++ packages/frontend/src/components/useMicRecorder.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect, useRef } from 'react';
+
+export const useMicRecorder = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  //const [audioBlob, setAudioBlob] = useState<Blob>();
+  const [audioURL, setAudioURL] = useState<string>('');
+  const mediaRecorderRef = useRef<MediaRecorder>();
+  const audioChunksRef = useRef<Blob[]>([]);
+
+  useEffect(() => {
+    if (isRecording) {
+      const startRecording = async () => {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+          });
+          mediaRecorderRef.current = new MediaRecorder(stream);
+
+          mediaRecorderRef.current.ondataavailable = event => {
+            audioChunksRef.current.push(event.data);
+          };
+
+          mediaRecorderRef.current.onstop = () => {
+            const blob = new Blob(audioChunksRef.current, {
+              type: 'audio/webm',
+            });
+            const audioURL = URL.createObjectURL(blob);
+            setAudioURL(audioURL);
+            //setAudioBlob(blob);
+
+            audioChunksRef.current = [];
+          };
+
+          mediaRecorderRef.current.start();
+        } catch (err) {
+          console.error('Error accessing microphone:', err);
+        }
+      };
+
+      startRecording();
+    } else {
+      if (mediaRecorderRef.current) {
+        mediaRecorderRef.current.stop();
+      }
+    }
+  }, [isRecording]);
+
+  const startRecording = () => {
+    setIsRecording(true);
+  };
+
+  const stopRecording = () => {
+    setIsRecording(false);
+  };
+
+  return { isRecording, startRecording, stopRecording, audioURL };
+};
