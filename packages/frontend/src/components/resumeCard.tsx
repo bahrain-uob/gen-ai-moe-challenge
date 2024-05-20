70f3ae3c5a201e9c09f13e443d437dfea0c1fb21
iamDanaK <daanaa227700@gmail.com>
2024-05-20 19:52:28 +0300

    - added shadows to cards - removed animamtion - fixed padding - fixed button size on mobile

--- packages/frontend/src/components/resumeCard.tsx
+++ packages/frontend/src/components/resumeCard.tsx
@@ -11,7 +11,7 @@ const resumeCard = ({
   percentage: number;
 }) => {
   return (
-    <div className=" w-[28rem] h-96 bg-white rounded-lg hover:w-[30rem] hover:h-[26rem] duration-500">
+    <div className=" w-[28rem] h-96 bg-white rounded-lg shadow-lg flex flex-col items-center">
       <div className="w-full h-1/6 flex justify-center items-center">
         <h3 className="text-3xl font-bold text-center max-sm:text-lg">
           {title}
@@ -22,7 +22,7 @@ const resumeCard = ({
         <CircularBar percentage={percentage} circleWidth={110} />
       </div>
 
-      <div className="w-full h-1/6 flex justify-center items-center">
+      <div className="w-full h-1/6 flex justify-center items-center pt-3 pb-10 px-6">
         <Button label="Resume" tag="3B828E" />
       </div>
     </div>
--- packages/frontend/src/sections/continuePast.tsx
+++ packages/frontend/src/sections/continuePast.tsx
@@ -2,14 +2,14 @@ import ResumeCard from '../components/resumeCard';
 
 const continuePast = () => {
   return (
-    <section className="h-full pb-6">
+    <section className="h-full pb-8">
       <div className="w-full h-1/6 px-4 pb-8">
         <div className="h-2/3 w-full">
           <h3 className="text-[65px] font-extrabold text-[#363534] max-lg:text-[55px]">
             Continue
           </h3>
         </div>
-        <div className="h-2/3 w-full">
+        <div className="h-2/3 w-full mb-2">
           <div className="h-3 w-4/12 bg-[#74ACB5] max-md:w-10/12"></div>
         </div>
       </div>
