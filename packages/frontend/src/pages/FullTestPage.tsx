f76ee44f3fb200b09af43f825ff7f6329769bbbf
mzhubail <mzhubail@gmail.com>
2024-05-20 21:19:13 +0300

    You cannot be just loading forever :)

--- packages/frontend/src/pages/FullTestPage.tsx
+++ packages/frontend/src/pages/FullTestPage.tsx
@@ -31,6 +31,7 @@ export const FullTestPage = () => {
         console.log('Recieved data');
         setState(response);
       }
+      setIsloading(false);
     },
     shouldReconnect: () => true,
   });
