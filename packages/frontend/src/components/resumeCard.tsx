15986ef4e4aee2ea48762ad38b43bbf420e9ae1d
iamDanaK <daanaa227700@gmail.com>
2024-05-20 20:15:04 +0300

    minor changes

--- packages/frontend/src/components/resumeCard.tsx
+++ packages/frontend/src/components/resumeCard.tsx
@@ -11,9 +11,9 @@ const resumeCard = ({
   percentage: number;
 }) => {
   return (
-    <div className=" w-[28rem] h-96 bg-white rounded-lg shadow-lg flex flex-col items-center">
+    <div className=" w-[28rem] h-96 bg-white rounded-lg shadow-lg flex flex-col items-center pt-6">
       <div className="w-full h-1/6 flex justify-center items-center">
-        <h3 className="text-3xl font-bold text-center max-sm:text-lg">
+        <h3 className="text-2xl font-bold text-center max-sm:text-lg">
           {title}
         </h3>
       </div>
@@ -22,7 +22,7 @@ const resumeCard = ({
         <CircularBar percentage={percentage} circleWidth={110} />
       </div>
 
-      <div className="w-full h-1/6 flex justify-center items-center pt-3 pb-10 px-6">
+      <div className="w-full h-1/6 flex justify-center items-center pt-7 pb-14 px-6">
         <Button label="Resume" tag="3B828E" />
       </div>
     </div>
