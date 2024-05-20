f1f80bc2b35b127bd07a5ce2fc4d2c36cf3468ac
iamDanaK <daanaa227700@gmail.com>
2024-05-20 19:52:28 +0300

    - added shadows to cards - removed animamtion - fixed padding - fixed button size on mobile

--- packages/frontend/src/components/FButton.tsx
+++ packages/frontend/src/components/FButton.tsx
@@ -1,7 +1,7 @@
 const Button = ({ label, tag }: { label: string; tag: string }) => {
   return (
     <button
-      className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-[#${tag}] rounded-full font-bold font-semi`}
+      className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-[#${tag}] rounded-full font-bold font-semi max-sm:py-3 max-sm:px-6`}
     >
       <p className="text-white max-sm:text-sm">{label}</p>
     </button>
