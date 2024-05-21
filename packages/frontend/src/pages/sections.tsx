3bf9f262c1278eff3dae89b20729e1c0918bbb82
thatishero <hasanrazi2003@gmail.com>
2024-05-21 19:06:50 +0300

    Hide every thing from the page by using a hidden div

--- packages/frontend/src/pages/sections.tsx
+++ packages/frontend/src/pages/sections.tsx
@@ -3,41 +3,43 @@ import Button from '../components/TButton';
 
 const sections = () => (
   <>
-    <h4 className="text-xl mb-4"> Better pages </h4>
-    <div className="flex">
-      <Link to="/_writing-task1">
-        <Button label="Writing Task 1" />
+    <div className="hidden">
+      <h4 className="text-xl mb-4"> Better pages </h4>
+      <div className="flex">
+        <Link to="/_writing-task1">
+          <Button label="Writing Task 1" />
+        </Link>
+        <Link to="/_writing-task2">
+          <Button label="Writing Task 2" />
+        </Link>
+      </div>
+      <h4 className="text-xl mb-4"> Dummy pages </h4>
+      <div className="flex">
+        <Link to="">
+          <Button label="Listening" />
+        </Link>
+        <Link to="/reading/1">
+          <Button label="Reading" />
+        </Link>
+        <Link to="/speaking">
+          <Button label="Speaking" />
+        </Link>
+      </div>
+      <div className="flex mt-6">
+        <Link to="/test-speaking-audio-ui">
+          <Button label="Speaking Audio UI" />
+        </Link>
+        <Link to="/test-speaking-card-ui">
+          <Button label="Speaking Card UI" />
+        </Link>
+      </div>
+      <Link to="/test">
+        <Button label="/test" />
       </Link>
-      <Link to="/_writing-task2">
-        <Button label="Writing Task 2" />
+      <Link to="/full-test">
+        <Button label="full test" />
       </Link>
     </div>
-    <h4 className="text-xl mb-4"> Dummy pages </h4>
-    <div className="flex">
-      <Link to="">
-        <Button label="Listening" />
-      </Link>
-      <Link to="/reading/1">
-        <Button label="Reading" />
-      </Link>
-      <Link to="/speaking">
-        <Button label="Speaking" />
-      </Link>
-    </div>
-    <div className="flex mt-6">
-      <Link to="/test-speaking-audio-ui">
-        <Button label="Speaking Audio UI" />
-      </Link>
-      <Link to="/test-speaking-card-ui">
-        <Button label="Speaking Card UI" />
-      </Link>
-    </div>
-    <Link to="/test">
-      <Button label="/test" />
-    </Link>
-    <Link to="/full-test">
-      <Button label="full test" />
-    </Link>
   </>
 );
 
