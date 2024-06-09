982a0823f37b9c3cab6175f9dc0344b22afed1df
mzhubail <mzhubail@gmail.com>
2024-06-09 17:19:25 +0300

    Major changes in feedback page
    - Page is integrated with backend
    - Added screens.  For this page, navigation between pages in handled by
      `screen` state, instead of react-router.

--- packages/frontend/src/components/AllFeedbacks.tsx
+++ packages/frontend/src/components/AllFeedbacks.tsx
@@ -1,36 +1,103 @@
 import React, { useEffect, useState } from 'react';
 import { FullTestItem } from '../../../functions/src/utilities/fullTestUtilities';
-import { post } from 'aws-amplify/api';
+import { Button } from './Button';
+import { get } from 'aws-amplify/api';
 import { toJSON } from '../utilities';
 import { Spinner } from './Spinner';
+import { useNavigate, useParams } from 'react-router-dom';
+import { WSFeedbackComponent } from './WSFeedback';
+
+type Screens = 'feedback' | 'listening' | 'reading' | 'writing' | 'speaking';
+type DataType = FullTestItem | undefined | { message: string };
 
 export const AllFeedbacks: React.FC = () => {
-  const [fullTestItem, setFullTestItem] = useState<
-    FullTestItem | undefined | null
-  >();
+  const [data, setData] = useState<DataType>();
+  const [screen, setScreen] = useState<Screens>('feedback');
+
+  const { sk } = useParams();
+  const navigate = useNavigate();
+  if (!sk) {
+    navigate(-1);
+  }
 
   useEffect(() => {
-    const getData = async () => {
-      const response = await toJSON(
-        post({
-          apiName: 'myAPI',
-          path: '/',
-        }),
-      );
-
-      setFullTestItem(response);
-    };
-    getData();
-    // return () => {};
+    console.log(`/fullTestFeedback/${sk}`);
+    toJSON(
+      get({
+        apiName: 'myAPI',
+        path: `/fullTestFeedback/${sk}`,
+      }),
+    )
+      .then(response => setData(response))
+      .catch(err => {
+        if ('message' in err) {
+          setData(err);
+        } else {
+          throw err;
+        }
+      });
   }, []);
 
   let out;
 
-  if (fullTestItem === undefined) {
-    out = <Spinner message={'Loading your feedback'} />;
-  } else {
-    out = JSON.stringify(fullTestItem);
+  if (!data) {
+    return <Spinner message={'Loading your feedback'} />;
+  }
+  // return JSON.stringify(fullTestItem);
+
+  if ('message' in data) {
+    return `Recieved error: ${data.message}`;
   }
 
-  return out;
+  switch (screen) {
+    case 'feedback':
+      out = (
+        <div>
+          <h3 className="text-lg font-light">Feedback</h3>
+          <div>
+            <Button onClick={() => setScreen('listening')}>
+              Listening Feedback
+            </Button>
+            <Button onClick={() => setScreen('reading')}>
+              Reading Feedback
+            </Button>
+            <Button onClick={() => setScreen('writing')}>
+              Writing Feedback
+            </Button>
+            <Button onClick={() => setScreen('speaking')}>
+              Speaking Feedback
+            </Button>
+          </div>
+        </div>
+      );
+      break;
+
+    case 'listening':
+      out = 'Listening';
+      break;
+
+    case 'reading':
+      out = 'reading';
+      break;
+
+    case 'writing':
+      const feedback = data?.writingAnswer?.feedback;
+      if (!feedback) break;
+      out = <WSFeedbackComponent feedback={feedback[0]} />;
+      break;
+
+    case 'speaking':
+      break;
+  }
+
+  return (
+    <>
+      {out}
+      {screen !== 'feedback' && (
+        <div className="mt-20">
+          <Button onClick={() => setScreen('feedback')}>Back</Button>
+        </div>
+      )}
+    </>
+  );
 };
