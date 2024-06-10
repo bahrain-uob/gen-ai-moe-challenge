3ca84f73e6ae27d19fe825091d7ef1cba418e406
mzhubail <mzhubail@gmail.com>
2024-06-10 15:32:49 +0300

    Hide save and submit if no handler was passed

--- packages/frontend/src/components/TestComponents.tsx
+++ packages/frontend/src/components/TestComponents.tsx
@@ -21,12 +21,16 @@ export const TitleRow: React.FC<TitleRowProps> = ({
       </div>
       <div className="w-1/3 text-center font-light text-xl">{title}</div>
       <div className="w-1/3 nav-item flex-row justify-end">
-        <button onClick={onSave} className="mr-2">
-          Save <BsFloppy2 className="inline max-sm:hidden mr-4" />
-        </button>
-        <button onClick={onSubmit}>
-          Submit <BsCheckLg className="inline max-sm:hidden" />
-        </button>
+        {onSave && (
+          <button onClick={onSave} className="mr-2">
+            Save <BsFloppy2 className="inline max-sm:hidden mr-4" />
+          </button>
+        )}
+        {onSubmit && (
+          <button onClick={onSubmit}>
+            Submit <BsCheckLg className="inline max-sm:hidden" />
+          </button>
+        )}
       </div>
     </div>
   );
