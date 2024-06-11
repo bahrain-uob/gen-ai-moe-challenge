8b80cc8c6611f35f54a10086b61b8ee190284b86
thatishero <hasanrazi2003@gmail.com>
2024-06-11 18:50:27 +0300

    Resetting exercise page

--- packages/frontend/src/pages/Exercises.tsx
+++ packages/frontend/src/pages/Exercises.tsx
@@ -1,23 +1,6 @@
-import { Link } from 'react-router-dom';
-import Button from '../components/TButton';
-
 const Exercises = () => (
   <div className="flex">
-    <Link to="">
-      <Button label="Listening" />
-    </Link>
-    <Link to="">
-      <Button label="Reading" />
-    </Link>
-    <Link to="/speakingExercises">
-      <Button label="Speaking practcise" />
-    </Link>
-    <Link to="/writing-task1">
-      <Button label="Writing Task 1" />
-    </Link>
-    <Link to="/writing-task2">
-      <Button label="Writing Task 2" />
-    </Link>
+    <h1>Hello</h1>
   </div>
 );
 
