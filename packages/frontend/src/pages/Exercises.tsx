8bad90483ac81792859a7474f5d7bc6d86ed8dbb
thatishero <hasanrazi2003@gmail.com>
2024-06-11 19:05:52 +0300

    Adding Stepper

--- packages/frontend/src/pages/Exercises.tsx
+++ packages/frontend/src/pages/Exercises.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepButton from '@mui/material/StepButton';
 
 const buttonLabels = [
   'Listening',
@@ -11,11 +13,17 @@ const buttonLabels = [
 ] as const;
 type ButtonLabel = (typeof buttonLabels)[number];
 
+const Challenges = ['Challenge 1', 'Challenge 2', 'Challenge 3' , 'Challenge 4' , 'Challenge 5'];
+
 const Exercises: React.FC = () => {
+  const [activeStep, setActiveStep] = React.useState(0);
   const [activeButton, setActiveButton] = useState<ButtonLabel>('Listening');
 
   const handleClick = (button: ButtonLabel) => {
     setActiveButton(button);
+  };
+  const handleStep = (step: number) => () => {
+    setActiveStep(step);
   };
 
   return (
@@ -36,6 +44,17 @@ const Exercises: React.FC = () => {
             <h1 className="font-semibold">{button}</h1>
           </Button>
         ))}
+      </div>
+      <div className='w-1/2'>
+      <Stepper nonLinear activeStep={activeStep}>
+        {Challenges.map((label, index) => (
+          <Step key={label}>
+            <StepButton color="inherit" onClick={handleStep(index)}>
+              {label}
+            </StepButton>
+          </Step>
+        ))}
+      </Stepper>
       </div>
     </main>
   );
