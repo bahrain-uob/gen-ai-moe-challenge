8241030a3942b168c202b8b8a2da801b9c610812
Ma-hawaj <ma.hawaj@gmail.com>
2024-06-12 17:22:52 +0300

    Modified current exam card

--- packages/frontend/src/pages/fullExam.tsx
+++ packages/frontend/src/pages/fullExam.tsx
@@ -1,9 +1,18 @@
-import { previousTestsList } from '../../../functions/src/utilities/fullTestUtilities';
+import {
+  previousTestsList,
+  testType,
+} from '../../../functions/src/utilities/fullTestUtilities';
 import Card from '../sections/currentExamCard';
 import PastTests from '../sections/pastTests';
 import { getRelativeTime } from '../utilities';
 
-const fullExam = ({ list }: { list?: previousTestsList }) => {
+const fullExam = ({
+  list,
+  type = 'full',
+}: {
+  list?: previousTestsList;
+  type: testType | 'full';
+}) => {
   list = {
     inProgress: '1718023891366-41bbe8b1-38e5-40bf-8d85-14c0ca40b1b1',
     previous: [
@@ -31,10 +40,15 @@ const fullExam = ({ list }: { list?: previousTestsList }) => {
       <section className="w-full h-4/6 flex justify-center my-8">
         {list.inProgress ? (
           <Card
-            title="IELTS - Full Test"
-            remTime="34 : 21"
+            title={
+              'IELTS - ' +
+              type.charAt(0).toUpperCase() +
+              type.slice(1) +
+              ' Test'
+            }
+            // remTime="34 : 21"
             startDate={getRelativeTime(list.inProgress)}
-            timing="Remaining Time"
+            // timing="Remaining Time"
           />
         ) : (
           <h3 className="text-4xl font-bold">No test in progress</h3>
