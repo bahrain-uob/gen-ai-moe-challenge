7d82c0bd7a089331f5e1add2692706a51653821d
Ma-hawaj <ma.hawaj@gmail.com>
2024-06-15 19:38:31 +0300

    Trying to solve the build error

--- packages/frontend/src/components/resumeCard.tsx
+++ packages/frontend/src/components/resumeCard.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-
 import CircularBar from './circularProgressBar';
 import Button from './cardButton';
 
-const resumeCard = ({
+const ResumeCard = ({
   title,
   percentage,
 }: {
@@ -14,22 +13,18 @@ const resumeCard = ({
   return (
     <div className="w-[20rem] h-72  bg-white rounded-lg shadow-lg flex flex-col items-center pt-6 ">
       <div className="w-full h-1/6 flex justify-center items-center">
-        <h3 className="text-xl  text-center max-sm:text-lg">
-          {title}
-        </h3>
+        <h3 className="text-xl  text-center max-sm:text-lg">{title}</h3>
       </div>
 
       <div className="w-full h-4/6 flex justify-center items-center">
-        <CircularBar percentage={percentage} circleWidth={110} radius={15}/>
+        <CircularBar percentage={percentage} circleWidth={110} radius={15} />
       </div>
 
       <div className="w-full h-1/6 flex justify-center items-center pt-5 pb-20 px-6">
-      <Button label='Resume' size='small'>
-       
-        </Button>
+        <Button label="Resume" size="small"></Button>
       </div>
     </div>
   );
 };
 
-export default resumeCard;
+export default ResumeCard;
