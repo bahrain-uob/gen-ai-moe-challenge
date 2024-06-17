9255014574d28c93894208dc87a22cb8a34b0590
saraisa12 <130190530+saraisa12@users.noreply.github.com>
2024-06-17 03:10:57 +0300

    added a progress bar

--- packages/frontend/src/components/ProgressBar.tsx
+++ packages/frontend/src/components/ProgressBar.tsx
@@ -2,7 +2,7 @@ export const ProgressBar = ({ percentage = 0, isAnimated = false }) => {
   const width = (percentage * 100).toFixed(2) + '%';
 
   return (
-    <div className="bg-blue-1 flex rounded-xl">
+    <div className="bg-blue-1 flex rounded-xl w-full">
       <div
         className={`bg-blue-4 inline-block h-4 rounded-xl ${
           isAnimated ? 'transition-all duration-500' : ''
