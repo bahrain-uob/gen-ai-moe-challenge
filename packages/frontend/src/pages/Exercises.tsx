e75769c633224a337bb9ca0c481e08c13e2d1b29
thatishero <hasanrazi2003@gmail.com>
2024-06-22 16:57:52 +0300

    Checking something (No need to view it)

--- packages/frontend/src/pages/Exercises.tsx
+++ packages/frontend/src/pages/Exercises.tsx
@@ -114,6 +114,16 @@ const Exercises: React.FC = () => {
           What is it ?
         </h1>
       </div>
+      <div className='hidden'>
+        <Button variant="contained" color="primary">
+            Take Test
+          </Button>
+          <Stepper nonLinear activeStep={activeStep}>
+            <Step key={'Hello'}>
+              <StepButton></StepButton>
+            </Step>
+          </Stepper>
+      </div>
     </main>
   );
 };
@@ -140,3 +150,4 @@ const entryTitle = (title: string, score: number) => (
     </div>
   </>
 );
+
